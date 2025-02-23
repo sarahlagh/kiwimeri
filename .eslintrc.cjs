9384047cb83a21998aeee4207d896465db68679a
sarahla <sarah@laghribi.fr>
2025-02-23 15:46:40 +0100

    eslint fix + prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  ignorePatterns: ['src/locales/**'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+};
---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  plugins: ['prettier-plugin-organize-imports']
+};
