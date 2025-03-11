de0ab858600fcf10efc6a9cad3c3d412ece7adc6
sarahla <sarah@laghribi.fr>
2025-03-11 11:55:28 +0100

    build electron app

--- electron/capacitor.config.ts
+++ electron/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.kiwimeri',
-  appName: 'kiwimeriApp',
+  appName: 'Kiwimeri',
   webDir: 'dist',
   plugins: {
     StatusBar: {
