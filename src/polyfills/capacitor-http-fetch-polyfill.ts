ab427044bfceb704ac4d4b7c05615ec03bf220a2
sarahla <sarah@laghribi.fr>
2025-04-07 15:27:25 +0200

    don't redirect svg requests to capacitor http plugin

--- src/polyfills/capacitor-http-fetch-polyfill.ts
+++ src/polyfills/capacitor-http-fetch-polyfill.ts
@@ -1,8 +1,15 @@
 import { Capacitor, CapacitorHttp } from '@capacitor/core';
 
+const origFetch = window.fetch;
 if (Capacitor.getPlatform() === 'android') {
   window.fetch = async (...args) => {
     const [resource, config] = args;
+    if (
+      !resource.toString().startsWith('http://') &&
+      !resource.toString().startsWith('https://')
+    ) {
+      return origFetch(resource, config);
+    }
     const options = {
       url: resource.toString(),
       ...config
