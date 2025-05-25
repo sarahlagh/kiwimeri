3c7831d436f395b9e33f276b3e51d392c067502d
sarahla <sarah@laghribi.fr>
2025-05-25 17:18:31 +0200

    delete page from ui

--- src/collection/components/CommonActionsToolbar.tsx
+++ src/collection/components/CommonActionsToolbar.tsx
@@ -1,12 +1,18 @@
+import { CollectionItemType } from '@/collection/collection';
 import CloseDocumentButton from '@/common/buttons/CloseDocumentButton';
 import DeleteItemButton from '@/common/buttons/DeleteItemButton';
 import MoveFolderButton from '@/common/buttons/MoveFolderButton';
 import RenameItemButton from '@/common/buttons/RenameItemButton';
+import { GET_DOCUMENT_ROUTE, GET_FOLDER_ROUTE } from '@/common/routes';
 import { APPICONS } from '@/constants';
+import collectionService from '@/db/collection.service';
+import userSettingsService from '@/db/user-settings.service';
 import { IonButton, IonButtons, IonIcon, IonToolbar } from '@ionic/react';
 
 export type CommonActionsToolbarProps = {
   id: string;
+  docId: string;
+  // pageId?: string;
   rows?: number;
   onClose: (role?: string, data?: unknown) => void;
   showRename?: boolean;
@@ -16,20 +22,34 @@ export type CommonActionsToolbarProps = {
 
 const CommonActionsToolbar = ({
   id,
+  docId,
   rows = 1,
   showRename = false,
   showClose = false,
   showInfo = false,
-  onClose,
-  children
+  onClose
 }: CommonActionsToolbarProps) => {
+  const type = collectionService.getItemType(id);
+  const showMoveFolder = type !== CollectionItemType.page;
+  showRename = type !== CollectionItemType.page && showRename;
+  showInfo = type !== CollectionItemType.page && showInfo;
+
+  const folder = userSettingsService.getCurrentFolder();
+  const fallbackRoute =
+    type !== CollectionItemType.page
+      ? GET_FOLDER_ROUTE(folder)
+      : GET_DOCUMENT_ROUTE(folder, docId);
+
   return (
     <IonToolbar color="medium" style={{ height: rows * 56 + 'px' }}>
       <IonButtons slot="end">
-        {children}
         {showRename && <RenameItemButton id={id} onClose={onClose} />}
-        <MoveFolderButton id={id} onClose={onClose} />
-        <DeleteItemButton id={id} onClose={onClose} />
+        {showMoveFolder && <MoveFolderButton id={id} onClose={onClose} />}
+        <DeleteItemButton
+          id={id}
+          fallbackRoute={fallbackRoute}
+          onClose={onClose}
+        />
 
         {showInfo && (
           <IonButton
@@ -42,7 +62,7 @@ const CommonActionsToolbar = ({
           </IonButton>
         )}
 
-        {showClose && <CloseDocumentButton id={id} onClose={onClose} />}
+        {showClose && <CloseDocumentButton id={docId} onClose={onClose} />}
       </IonButtons>
     </IonToolbar>
   );
