7370a858903b5c37f66725d79689ae6bcbb0cc3f
sarahla <sarah@laghribi.fr>
2025-05-27 20:46:16 +0200

    use transformers

--- src/format-conversion/formatter.ts
+++ src/format-conversion/formatter.ts
@@ -1,18 +1,53 @@
-import { SerializedLexicalNode } from 'lexical';
+import { SerializedElementNode, SerializedLexicalNode } from 'lexical';
 
 export type KiwimeriTransformer = {
   type: string;
+  handles?: (node: SerializedLexicalNode) => boolean;
+  transform?: (text: string, opts?: unknown) => string;
+  preTransform?: (
+    fullstr: string,
+    hasChildren: boolean,
+    opts?: unknown
+  ) => string;
+  postTransform?: (
+    fullstr: string,
+    hasChildren: boolean,
+    opts?: unknown
+  ) => string;
 };
 
 export abstract class KiwimeriFormatter {
-  protected transformers: KiwimeriTransformer[] = [];
+  constructor(protected transformers: KiwimeriTransformer[]) {}
 
-  public abstract transform(
-    node: SerializedLexicalNode,
-    opts?: unknown,
-    index?: number
-  ): string;
+  public stringifyLexNode(node: SerializedLexicalNode, opts?: unknown) {
+    let text = '';
+    const transformer = this.transformers.find(
+      ({ type, handles }) =>
+        type === node.type && (handles ? handles(node) : true)
+    );
+    if ('children' in node) {
+      const elementNode = node as SerializedElementNode;
+      const hasChildren = elementNode.children.length > 0;
 
-  public abstract transformStart(): string;
-  public abstract transformEnd(text?: string): string;
+      if (transformer?.preTransform) {
+        text = transformer.preTransform(text, hasChildren, opts);
+      }
+      elementNode.children.forEach(child => {
+        text += this.stringifyLexNode(child, opts);
+      });
+      if (transformer?.postTransform) {
+        text = transformer.postTransform(text, hasChildren, opts);
+      }
+    }
+    if ('text' in node) {
+      if (transformer?.transform) {
+        text += transformer.transform(node.text as string, opts);
+      } else {
+        text += node.text;
+      }
+    } else if (transformer?.transform) {
+      text += transformer.transform('', opts);
+    }
+    return text;
+  }
 }
--- src/vitest/unit/format-conversion/paragraph/test.md
+++ src/vitest/unit/format-conversion/paragraph/test.md
@@ -3,4 +3,6 @@ This is a **simple** test with some *basic* style.
 This is a <u>test</u> with a ~~paragraph~~ break.
 And a carriage return.
 
+
 And a double one
+
