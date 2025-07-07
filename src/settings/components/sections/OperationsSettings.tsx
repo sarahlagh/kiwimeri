eccea822906ef5a406519c6b520a2f9057360ef3
sarahla <sarah@laghribi.fr>
2025-07-07 16:39:27 +0200

    generic import file button

--- src/settings/components/sections/OperationsSettings.tsx
+++ src/settings/components/sections/OperationsSettings.tsx
@@ -2,7 +2,13 @@ import localChangesService from '@/db/localChanges.service';
 import notebooksService from '@/db/notebooks.service';
 import storageService from '@/db/storage.service';
 import tagsService from '@/db/tags.service';
-import { IonButton, IonCard, IonCardHeader, IonCardTitle } from '@ionic/react';
+import {
+  IonButton,
+  IonButtons,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle
+} from '@ionic/react';
 import { Trans } from '@lingui/react/macro';
 
 const OperationSettings = () => {
@@ -14,47 +20,49 @@ const OperationSettings = () => {
         </IonCardTitle>
       </IonCardHeader>
 
-      <IonButton
-        fill="clear"
-        onClick={() => {
-          storageService.getStore().setContent([{}, {}]);
-        }}
-        color="danger"
-      >
-        <Trans>nuke store</Trans>
-      </IonButton>
-      <IonButton
-        fill="clear"
-        onClick={() => {
-          storageService.getSpace().setContent([{}, {}]);
-          localChangesService.clear();
-          tagsService.reBuildTags();
-          notebooksService.initNotebooks();
-        }}
-        color="danger"
-      >
-        <Trans>nuke space</Trans>
-      </IonButton>
-      <IonButton
-        fill="clear"
-        onClick={() => {
-          storageService.getSpace().setContent([{}, {}]);
-          localChangesService.clear();
-          storageService.getStore().setCell('remoteState', '0', 'info', '{}');
-          storageService
-            .getStore()
-            .setCell('spaces', 'default', 'lastLocalChange', 0);
-          storageService
-            .getStore()
-            .setCell('remoteState', '0', 'lastRemoteChange', 0);
-          storageService.getStore().delTable('remoteItems');
-          tagsService.reBuildTags();
-          notebooksService.initNotebooks();
-        }}
-        color="danger"
-      >
-        <Trans>nuke space & remote</Trans>
-      </IonButton>
+      <IonButtons>
+        <IonButton
+          fill="clear"
+          onClick={() => {
+            storageService.getStore().setContent([{}, {}]);
+          }}
+          color="danger"
+        >
+          <Trans>nuke store</Trans>
+        </IonButton>
+        <IonButton
+          fill="clear"
+          onClick={() => {
+            storageService.getSpace().setContent([{}, {}]);
+            localChangesService.clear();
+            tagsService.reBuildTags();
+            notebooksService.initNotebooks();
+          }}
+          color="danger"
+        >
+          <Trans>nuke space</Trans>
+        </IonButton>
+        <IonButton
+          fill="clear"
+          onClick={() => {
+            storageService.getSpace().setContent([{}, {}]);
+            localChangesService.clear();
+            storageService.getStore().setCell('remoteState', '0', 'info', '{}');
+            storageService
+              .getStore()
+              .setCell('spaces', 'default', 'lastLocalChange', 0);
+            storageService
+              .getStore()
+              .setCell('remoteState', '0', 'lastRemoteChange', 0);
+            storageService.getStore().delTable('remoteItems');
+            tagsService.reBuildTags();
+            notebooksService.initNotebooks();
+          }}
+          color="danger"
+        >
+          <Trans>nuke space & remote</Trans>
+        </IonButton>
+      </IonButtons>
     </IonCard>
   );
 };
