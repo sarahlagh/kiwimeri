b04d4302067326bb1f0d21fece5b67a5216b267c
sarahla <sarah@laghribi.fr>
2025-07-09 07:16:48 +0200

    split import export settings

--- src/settings/components/sections/ImportExportCollectionSettings.tsx
+++ src/settings/components/sections/ImportExportCollectionSettings.tsx
@@ -1,23 +1,83 @@
+import GenericExportFileButton from '@/common/buttons/GenericExportFileButton';
+import GenericImportFileButton from '@/common/buttons/GenericImportFileButton';
 import platformService from '@/common/services/platform.service';
+import { ANDROID_FOLDER } from '@/constants';
 import storageService from '@/db/storage.service';
-import { useLingui } from '@lingui/react/macro';
-import ImportExportSettings from './ImportExportSettings';
+import {
+  IonButtons,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonText
+} from '@ionic/react';
+import { Trans, useLingui } from '@lingui/react/macro';
 
 const ImportExportCollectionSettings = () => {
   const { t } = useLingui();
+
+  const exportFileSuffix = `${platformService.getPlatform()}-backup`;
+  const getExportFileName = () =>
+    `${new Date().toISOString().substring(0, 19).replaceAll(/[:T]/g, '-')}-${exportFileSuffix}.json`;
+
+  const onRestoreContent = async (content: string) => {
+    const json = JSON.parse(content);
+    storageService.getSpace().setContent(json);
+  };
+
+  const getContentToExport = async () => {
+    return storageService.getSpace().getJson();
+  };
+
+  const onImportContentRead = async (content: string) => {
+    await onRestoreContent(content);
+    return true;
+  };
+
   return (
-    <ImportExportSettings
-      title={t`Import & export your collection`}
-      subtitle={t`Manually backup or restore your collection.`}
-      exportFileSuffix={`${platformService.getPlatform()}-backup`}
-      onRestoreContent={async (content: string) => {
-        const json = JSON.parse(content);
-        storageService.getSpace().setContent(json);
-      }}
-      getContentToExport={async () => {
-        return storageService.getSpace().getJson();
-      }}
-    />
+    <IonCard>
+      <IonCardHeader>
+        <IonCardTitle>
+          <Trans>Import & export your collection</Trans>
+        </IonCardTitle>
+        <IonCardSubtitle>
+          <Trans>Manually backup or restore your collection</Trans>
+        </IonCardSubtitle>
+      </IonCardHeader>
+
+      {platformService.isAndroid() && (
+        <IonCardContent>
+          <IonText color={'secondary'}>
+            <p>
+              <Trans>
+                Your backups will be exported to the `{ANDROID_FOLDER}`
+                directory
+              </Trans>
+            </p>
+          </IonText>
+        </IonCardContent>
+      )}
+
+      <IonButtons>
+        <GenericExportFileButton
+          fill="clear"
+          color={'primary'}
+          label={t`Export`}
+          icon={null}
+          fileMime={'application/json'}
+          getFileTitle={getExportFileName}
+          getFileContent={getContentToExport}
+        />
+        <GenericImportFileButton
+          fill="clear"
+          color="danger"
+          icon={null}
+          label={t`Restore`}
+          onContentReadAsString={onImportContentRead}
+        />
+      </IonButtons>
+    </IonCard>
   );
 };
 export default ImportExportCollectionSettings;
