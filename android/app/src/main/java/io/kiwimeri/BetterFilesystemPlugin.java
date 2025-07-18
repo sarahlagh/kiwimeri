be5c70971f42f5600ea379ded35b3658a90fd19f
sarahla <sarah@laghribi.fr>
2025-07-18 11:30:06 +0200

    handle permissions

--- android/app/src/main/java/io/kiwimeri/BetterFilesystemPlugin.java
+++ android/app/src/main/java/io/kiwimeri/BetterFilesystemPlugin.java
@@ -1,36 +1,56 @@
 package io.kiwimeri;
 
+import android.Manifest;
+import android.os.Build;
 import android.os.Environment;
 import android.util.Base64;
 
 import com.getcapacitor.JSObject;
+import com.getcapacitor.PermissionState;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import com.getcapacitor.annotation.Permission;
+import com.getcapacitor.annotation.PermissionCallback;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-@CapacitorPlugin(name = "BetterFilesystem")
+@CapacitorPlugin(name = "BetterFilesystem", permissions = {
+        @Permission(
+                strings = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                alias = "storage"
+        )
+})
 public class BetterFilesystemPlugin extends Plugin {
 
     @PluginMethod()
     public void exportToFile(PluginCall call) {
         String fileName = call.getString("fileName");
         String content = call.getString("content");
-        String appDir = call.getString("appDir", "KiwimeriApp");
 
+        // TODO file picker
+        // TODO handle streaming
         if (fileName == null || content == null) {
             call.reject("missing parameter fileName or content");
             return;
         }
 
-        // TODO handle permissions request
-        // TODO file picker
+        if (!isStoragePermissionGranted()) {
+            requestAllPermissions(call, "permsCallback");
+        } else {
+            exportToFileCallback(call);
+        }
 
+    }
+
+    private void exportToFileCallback(PluginCall call) {
+        String fileName = call.getString("fileName");
+        String content = call.getString("content");
+        String appDir = call.getString("appDir", "KiwimeriApp");
         try {
             File androidDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
             if (androidDir.exists() || androidDir.mkdirs()) {
@@ -61,5 +81,18 @@ public class BetterFilesystemPlugin extends Plugin {
             }
         }
         call.resolve(new JSObject().put("success", true));
+    }
+
+    private boolean isStoragePermissionGranted() {
+        return Build.VERSION.SDK_INT >= 30 || getPermissionState("storage") == PermissionState.GRANTED;
+    }
+
+    @PermissionCallback
+    private void permsCallback(PluginCall call) {
+        if (isStoragePermissionGranted()) {
+            exportToFileCallback(call);
+        } else {
+            call.reject("Permission is required to write to storage");
+        }
     }
 }
