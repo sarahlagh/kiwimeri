31797e6638227fe1600e002718cd188a0806dc6f
sarahla <sarah@laghribi.fr>
2026-03-25 09:40:55 +0100

    revert hack for electron

--- scripts/release-electron.sh
+++ scripts/release-electron.sh
@@ -16,16 +16,9 @@ fi
 
 echo "building the app for production"
 
-# ugly hack to get electron release to work
-# quick win before I upgrade to cap 8 AND stop using @capacitor-community/electron
-mv capacitor.config.ts capacitor.config.ts.bak
-cp electron/capacitor.config.ts .
-
 yarn --cwd=electron install --frozen-lockfile
 npx ionic cap build electron --no-open --prod
 yarn --cwd=electron electron:make
-
-mv capacitor.config.ts.bak capacitor.config.ts
 
 if [ -f ".env.production.local.bak" ]; then
     mv .env.production.local.bak .env.production.local
