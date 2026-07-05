3662a54fa1bee8e9692df345e7579b8ceb43cff6
sarahla <sarah@laghribi.fr>
2026-07-05 11:05:57 +0200

    fix test

--- test/browser/App.test.tsx
+++ test/browser/App.test.tsx
@@ -1,12 +1,9 @@
 import App from '@/App';
 import { expect, test } from 'vitest';
 import { render } from 'vitest-browser-react';
-import { TestingProvider } from './TestingProvider';
 
 test('renders without crashing', async () => {
-  const screen = await render(<App />, {
-    wrapper: TestingProvider
-  });
+  const screen = await render(<App />);
 
   expect(screen.baseElement).toBeDefined();
 });
