3e74c76f941db625c3c48ed3c07fab73092a31e9
sarahla <sarah@laghribi.fr>
2026-08-24 07:29:13 +0200

    revert content listeners

--- src/domain/collection/derived-tables-listeners.ts
+++ src/domain/collection/derived-tables-listeners.ts
@@ -1,10 +1,8 @@
-import { appConfig } from '@/config';
 import { DOC_PREVIEW_SIZE } from '@/constants';
 import { space, spaceDocContent } from '@/core/db/store';
 import { SpaceDocContentTables, SpaceTables } from '@/core/db/store-constants';
 import { SpaceDocContentTableId, SpaceTableId } from '@/core/db/store-schema';
 import { MetaField } from '@/core/db/types';
-import { schedule } from '@/core/tasks/scheduler.service';
 import { getPlainText } from '@/shared/misc/getPlainText';
 import { Id } from 'tinybase/with-schemas';
 import { statsOnPlainTextCallback } from '../stats/stats-on-change-callback';
@@ -20,37 +18,32 @@ function addDerivedContentListener(
     content_meta: MetaField
   ) => void
 ) {
-  const taskName = 'calc_' + contentTableId;
-  schedule.register(taskName, inputs => {
-    if (!inputs) return;
-    const { rowId } = inputs;
-    const content = spaceDocContent.getCell(contentTableId, rowId, 'content');
-    if (!content) return;
-    const plainText = getPlainText(content as string);
-    const previewText = plainText.substring(0, DOC_PREVIEW_SIZE);
-    space.setRow(viewTableId, rowId, {
-      previewText
-    });
-    spaceDocContent.setCell(contentTableId, rowId, 'plainText', plainText);
-    const content_meta = spaceDocContent.getCell(
-      contentTableId,
-      rowId,
-      'content_meta'
-    ) as MetaField;
-    if (onPlainTextChange) onPlainTextChange(rowId, plainText, content_meta);
-  });
-
   contentListeners.push(
     spaceDocContent.addCellListener(
       contentTableId,
       null,
       'content',
       (_store, tableId, rowId, cellId, newCell) => {
-        // if (!newCell || cellEquals(newCell, oldCell)) return;
-        if (!newCell) return;
-        schedule.in(appConfig.WRITER_DERIVED_STATS_THROTTLE, taskName, {
-          rowId
-        });
+        if (newCell) {
+          const plainText = getPlainText(newCell);
+          const previewText = plainText.substring(0, DOC_PREVIEW_SIZE);
+          space.setRow(viewTableId, rowId, {
+            previewText
+          });
+          spaceDocContent.setCell(
+            contentTableId,
+            rowId,
+            'plainText',
+            plainText
+          );
+          const content_meta = spaceDocContent.getCell(
+            contentTableId,
+            rowId,
+            'content_meta'
+          ) as MetaField;
+          if (onPlainTextChange)
+            onPlainTextChange(rowId, plainText, content_meta);
+        }
       },
       true
     )
